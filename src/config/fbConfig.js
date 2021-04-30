e467ea6c7c3254c8e899201d225b59191eaef946
Musa <black.nobody.anb@gmail.com>
2021-04-30 19:18:53 +0600

    added firebase package and created config file

---
+++ src/config/fbConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDFBRkBMuGbkynXbSMArXvCfsN8FXDPE-M",
+  authDomain: "fundraising-d9bc9.firebaseapp.com",
+  databaseURL: "https://fundraising-d9bc9-default-rtdb.firebaseio.com",
+  projectId: "fundraising-d9bc9",
+  storageBucket: "fundraising-d9bc9.appspot.com",
+  messagingSenderId: "913745854857",
+  appId: "1:913745854857:web:a7b82416e3918a520ba158",
+};
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
