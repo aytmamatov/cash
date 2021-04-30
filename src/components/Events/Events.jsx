fb01a17b2c84a668e1ce90983a7b8e1753b8615f
Musa <black.nobody.anb@gmail.com>
2021-04-30 22:05:47 +0600

    made the first request for events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { instance } from "../../api/api";
 
 function Events() {
+  const [events, setEvents] = useState({});
+  useEffect(() => {
+    instance.get("events.json").then(({ data }) => setEvents(data));
+  }, []);
   return (
     <div className="events">
       <div className="events__top">
-        <Link to='/create-event' className="btn btn-primary">Создать мероприятие</Link>
+        <Link to="/create-event" className="btn btn-primary">
+          Создать мероприятие
+        </Link>
+      </div>
+      <div className="events__main">
+        {Object.keys(events).length > 0
+          ? Object.keys(events).map((event) => {
+              return (
+                <Card key={event}>
+                  <Card.Text>{events[event].nameEvent}</Card.Text>
+                </Card>
+              );
+            })
+          : null}
       </div>
     </div>
   );
