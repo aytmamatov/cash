bfe95e73e193be76c00b8891bc42031455cbf46b
Musa <black.nobody.anb@gmail.com>
2021-05-01 17:09:40 +0600

    fixed bugs and [200~supplemented the event card

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { instance } from "../../api/api";
-
+import "./Events.sass";
 function Events() {
   const [events, setEvents] = useState({});
   useEffect(() => {
@@ -21,8 +21,22 @@ function Events() {
         {Object.keys(events).length > 0
           ? Object.keys(events).map((event) => {
               return (
-                <Card key={event}>
-                  <Card.Text>{events[event].nameEvent}</Card.Text>
+                <Card key={event} className="event">
+                  <div className="event__sidebar">
+                    <Card.Title>
+                      Мероприятие <b>{events[event].nameEvent}</b>
+                    </Card.Title>
+                    <Card.Text>
+                      Дата {events[event].startTime} / {events[event].endTime}
+                    </Card.Text>
+                    <Card.Text>Статус: активный</Card.Text>
+                  </div>
+                  <Link
+                    to={`/events/${event}`}
+                    className="btn btn-primary event__link-more"
+                  >
+                    Подробнее
+                  </Link>
                 </Card>
               );
             })
