8214515643a18fb18f8639ecae55c87d62868e07
Musa <black.nobody.anb@gmail.com>
2021-05-07 13:13:33 +0600

    made a deletion of the event

--- src/components/Events/FullEvent/FullEvent.jsx
+++ src/components/Events/FullEvent/FullEvent.jsx
@@ -1,29 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { Button } from "react-bootstrap";
+import { useHistory, useParams } from "react-router";
 import { instance } from "../../../api/api";
 import Preloader from "../../UI/Preloader/Preloader";
-
+import firebase from "./../../../config/fbConfig";
+import "./FullEvent.sass";
 function FullEvent() {
+  const history = useHistory();
   const { id } = useParams();
-  const [isLoading, setIsLoading] = useState(true);
+  const [loadingData, setLoadingData] = useState({
+    isLoading: false,
+    isLoadingRemove: false,
+  });
   const [data, setData] = useState({});
+
   useEffect(() => {
     instance
       .get(`events/${id}.json`)
       .then(({ data }) => setData(data))
-      .then(() => setIsLoading(false));
+      .then(() => setLoadingData((prev) => ({ ...prev, isLoading: true })));
   }, []);
+  const removePostHandler = () => {
+    setLoadingData((prev) => ({ ...prev, isLoadingRemove: true }));
+    firebase
+      .database()
+      .ref(`events/${id}`)
+      .remove()
+      .then(() => history.push("/events"));
+  };
   return (
     <div>
-      {isLoading ? (
-        <Preloader />
-      ) : (
-        <div className="fullEvent">
-          <h3>{data.nameEvent}</h3>
-          <div className="fullEvent__date">
-            Дата {data.startTime} / {data.endTime}
+      {loadingData.isLoading ? (
+        loadingData.isLoadingRemove ? (
+          <Preloader title="Удаляется..." />
+        ) : (
+          <div className="fullEvent">
+            <h3>{data.nameEvent}</h3>
+            <div className="fullEvent__date">
+              Дата {data.startTime} / {data.endTime}
+            </div>
+            <div className="fullEvent__group">
+              <Button
+                onClick={removePostHandler}
+                variant="danger"
+                className="fullEvent__remove"
+              >
+                Удалить
+              </Button>
+            </div>
           </div>
-        </div>
+        )
+      ) : (
+        <Preloader />
       )}
     </div>
   );
